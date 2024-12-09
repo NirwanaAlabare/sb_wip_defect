6293c41b6997f818f626441a9c83f3dc11bae234
Rizky <rizkyaditya2106@gmail.com>
2024-12-09 08:24:20 +0700

    fix defect in unable to save selected defect

--- app/Http/Livewire/DefectInOut.php
+++ app/Http/Livewire/DefectInOut.php
@@ -44,7 +44,7 @@ class DefectInOut extends Component
     public $defectOutQty;
 
     public $defectOutDateModal;
-    public $defectOutOutputeModal;
+    public $defectOutOutputModal;
     public $defectOutLineModal;
     public $defectOutMasterPlanModal;
     public $defectOutSizeModal;
@@ -967,8 +967,8 @@ class DefectInOut extends Component
                 $defectInQuery->where("output_defects_packing.defect_type_id", $this->defectInTypeModal);
             }
         } else {
-            $defectInQuery = DefectPacking::selectRaw("
-                output_defects_packing.id as defect_id,
+            $defectInQuery = Defect::selectRaw("
+                output_defects.id as defect_id,
                 'defect' as status,
                 '".Auth::user()->Groupp."' as type,
                 'qc' as output_type,
@@ -976,15 +976,15 @@ class DefectInOut extends Component
                 '".Carbon::now()."' as created_at,
                 '".Carbon::now()."' as updated_at
             ")->
-            leftJoin("so_det", "so_det.id", "=", "output_defects_packing.so_det_id")->
-            leftJoin("master_plan", "master_plan.id", "=", "output_defects_packing.master_plan_id")->
+            leftJoin("so_det", "so_det.id", "=", "output_defects.so_det_id")->
+            leftJoin("master_plan", "master_plan.id", "=", "output_defects.master_plan_id")->
             leftJoin("act_costing", "act_costing.id", "=", "master_plan.id_ws")->
-            leftJoin("output_defect_types", "output_defect_types.id", "=", "output_defects_packing.defect_type_id")->
+            leftJoin("output_defect_types", "output_defect_types.id", "=", "output_defects.defect_type_id")->
             leftJoin("output_defect_in_out", function($join) {
-                $join->on("output_defect_in_out.defect_id", "=", "output_defects_packing.id");
+                $join->on("output_defect_in_out.defect_id", "=", "output_defects.id");
                 $join->on("output_defect_in_out.output_type", "=", DB::raw("'packing'"));
             })->
-            where("output_defects_packing.defect_status", "defect")->
+            where("output_defects.defect_status", "defect")->
             where("output_defect_types.allocation", Auth::user()->Groupp)->
             whereNull("output_defect_in_out.id");
             if ($this->defectInDateModal) {
@@ -997,10 +997,10 @@ class DefectInOut extends Component
                 $defectInQuery->where("master_plan.id", $this->defectInMasterPlanModal);
             }
             if ($this->defectInSizeModal) {
-                $defectInQuery->where("output_defects_packing.so_det_id", $this->defectInSizeModal);
+                $defectInQuery->where("output_defects.so_det_id", $this->defectInSizeModal);
             }
             if ($this->defectInTypeModal) {
-                $defectInQuery->where("output_defects_packing.defect_type_id", $this->defectInTypeModal);
+                $defectInQuery->where("output_defects.defect_type_id", $this->defectInTypeModal);
             }
         }
 
