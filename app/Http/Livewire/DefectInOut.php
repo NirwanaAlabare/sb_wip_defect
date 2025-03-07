619e21f83f8e20a9fb01bbc2848dbf93a2be19e1
it-nag <ramon@ptnag.com>
2025-03-07 09:23:44 +0700

    fix pre submit defect out empty data

--- app/Http/Livewire/DefectInOut.php
+++ app/Http/Livewire/DefectInOut.php
@@ -1311,15 +1311,13 @@ class DefectInOut extends Component
             so_det.size,
             COUNT(output_defects.id) defect_qty
         ")->
-        leftJoin(($this->defectOutOutputType == 'packing' ? 'output_defects_packing' : ($this->defectOutOutputType == 'qcf' ? 'output_check_finishing' : 'output_defects'))." as output_defects"." as output_defects", "output_defects.id", "=", "output_defect_in_out.defect_id")->
+        leftJoin(($thisData[3] == 'packing' ? 'output_defects_packing' : ($thisData[3] == 'qcf' ? 'output_check_finishing' : 'output_defects'))." as output_defects", "output_defects.id", "=", "output_defect_in_out.defect_id")->
         leftJoin("so_det", "so_det.id", "=", "output_defects.so_det_id")->
         leftJoin("master_plan", "master_plan.id", "=", "output_defects.master_plan_id")->
         leftJoin("act_costing", "act_costing.id", "=", "master_plan.id_ws")->
         leftJoin("output_defect_types", "output_defect_types.id", "=", "output_defects.defect_type_id")->
         where("output_defect_types.allocation", Auth::user()->Groupp)->
         where("output_defect_in_out.type", Auth::user()->Groupp)->
-        where("output_defect_in_out.output_type", $this->defectOutOutputType)->
-        where("output_defect_in_out.created_by", Auth::user()->username)->
         where("master_plan.id", $thisData[0])->
         where("output_defect_types.id", $thisData[1])->
         where("output_defects.so_det_id", $thisData[2])->
